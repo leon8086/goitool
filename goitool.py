4ae719f8f0ca9092fb59d8164ea5cbb4c45cc084
leon8086 <leon8086@hotmail.com>
2017-11-28 22:23:18 +0800

    change ui to en

--- goitool.py
+++ goitool.py
@@ -5,14 +5,6 @@ import _winreg
 import os,re,sys
 from Tkinter import *
 
-def IntKeyToNode( soup, key, subkey ):
-    node = soup.new_tag( "key" )
-    v, t = _winreg.QueryValueEx(key,subkey)
-    node.attrs['name'] = subkey
-    node.attrs['type'] = t
-    node.string = str(v)
-    return node
-
 def BinaryEncode( s ):
     ret = ""
     for c in s:
@@ -43,8 +35,9 @@ def LoadKey(out):
 
 def Save( ):
     dlg = Toplevel(root)
+    dlg.title("Save Name")
     rwid, rhei = root.winfo_x(), root.winfo_y()
-    geo = "250x50+%d+%d"%(rwid+25,rhei+120)
+    geo = "250x60+%d+%d"%(rwid+25,rhei+120)
     dlg.geometry(geo)
     dlg.filename=""
     dlg.resizable(width=False,height=False)
@@ -57,7 +50,7 @@ def Save( ):
             filename=filename+".dat"
             SaveKey(open(filename,"w"))
             UpdateList(listb)
-    btn = Button(dlg,text=u"确定", font=(u"宋体",12),command=lambda:Close())
+    btn = Button(dlg,text=u"(&O)K", font=(u"Arial",12),command=lambda:Close())
     btn.grid(row=1)
     root.wait_window(dlg)
 
@@ -79,17 +72,17 @@ def UpdateList( lb ):
 
 root = Tk()
 
-listb = Listbox(root,font=(u"宋体",12),width=37)
+listb = Listbox(root,font=(u"Arial",12),width=35)
 UpdateList(listb)
 
 listb.grid( row=0, column=0, columnspan=2)
-b_save = Button( root, text=u"存盘", font=(u"宋体",12), width=10, command=Save )
+b_save = Button( root, text=u"(&S)ave", font=(u"Arial",12), width=10, command=Save )
 b_save.grid( row=1, column=0 )
-b_load = Button( root, text=u"读取", font=(u"宋体",12), width=10, command=Load )
+b_load = Button( root, text=u"(&L)oad", font=(u"Arial",12), width=10, command=Load )
 b_load.grid( row=1, column=1 )
 root.title("Get Over It Save/Load")
 width = root.winfo_vrootwidth()+root.winfo_vrootx()
 height = root.winfo_vrootheight()+root.winfo_vrooty()
-root.geometry( "300x210+%d+%d"%(width/2-150,height/2-100) )
+root.geometry( "320x230+%d+%d"%(width/2-150,height/2-100) )
 root.resizable(width=False,height=False)
 root.mainloop()
